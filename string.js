3b5283746181353cb1421d107d3d175082cec513
mahamadi congo <mahamadicongo@hotmail.com>
2019-08-02 16:09:16 +0200

    switch statements

--- string.js
+++ string.js
@@ -218,3 +218,48 @@ function testStrictNotEqual(a) {
     return " Equal";
 }
 console.log(testStrictNotEqual(91));
+
+// Golf Code
+var names = ["Hole-in-one", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home"];
+function golfScore(par, strokes) {
+    if (strokes == 1) {
+        return names[0];
+    }else if (strokes <= par -2) {
+        return names [1]
+    }else if (strokes == par -1) {
+        return names [2]
+    }else if (strokes == par ) {
+        return names [3]
+    }else if (strokes == par + 1) {
+        return names [4]
+    }else if (strokes == par + 2) {
+        return names [5]
+    }else if (strokes >= par + 3) {
+        return names [6]
+    }
+}
+console.log(golfScore(5,8));
+
+// switch statements
+
+function caseInSwitch(val) {
+    var answer = "";
+    switch (val) {
+        case 1:
+            return answer = "Alpha";
+            break;
+        case 2:
+            return answer = "Beta";
+            break;
+        case 3:
+            return answer = "Gamma";
+            break;
+        case 4:
+            return answer = "Delta";
+            break;
+        default:
+            return answer = "what ?";
+            break;
+    }
+}
+console.log(caseInSwitch(2));
