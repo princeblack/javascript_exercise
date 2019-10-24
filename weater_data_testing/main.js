81afbb7fe2ac3bdd18c380c7c63d9610f2c9d739
princeblack <mahamadicongo@hotmail.com>
2019-10-25 01:55:48 +0200

    style with sass

--- weater_data_testing/main.js
+++ weater_data_testing/main.js
@@ -1,9 +1,5 @@
 let search = document.getElementById("search");
 const submit = document.getElementById("submit");
-submit.style.width="100%";
-search.style.width="99%";
-search.style.height="30px"
-search.style
 let city = "";
 let url=`http://api.openweathermap.org/data/2.5/weather?q=Berlin&APPID=7db45b5a58a7c1597b2bbae96008d4ad&units=metric`;
 
@@ -18,11 +14,13 @@ submit.onclick = ()=>{
         alert("please fill out the city name")
     }
 }
+
 search.addEventListener("keyup", function (e) {
     if (e.keyCode === 13 ) {
         submit.onclick();
     }    
 })
+
 //   get data forme the api 
 const performGetRequest = async () =>{
     try {
@@ -36,12 +34,10 @@ const performGetRequest = async () =>{
         console.log(error);
     }
 }
+performGetRequest()
 
 //  body background 
 const bodyBackground = (data)=>{
-    document.body.style.backgroundRepeat ="no-repeat";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundPosition = "bottom";
     
     // array of different description of weather
     let weaterDescr = ["mist","Smoke","Haze","sand/ dust whirls","fog","sand","dust","volcanic ash","squalls","tornado"]
@@ -68,7 +64,7 @@ const bodyBackground = (data)=>{
         document.body.style.backgroundSize = "cover";
         document.body.style.backgroundPosition = "bottom";
     }else if (data.weather[0].description.includes("rain")) {
-        document.body.style.background ="url('img/rain.jpg)";
+        document.body.style.background ="url('img/rain.jpg')";
         document.body.style.backgroundRepeat ="no-repeat";
         document.body.style.backgroundSize = "cover";
         document.body.style.backgroundPosition = "bottom";
@@ -93,7 +89,12 @@ const weather = (data)=>{
     icon.src = iconUrl;
     // weather city
     let city = document.getElementById("city");
-    city.innerHTML= data.name
+    city.innerHTML= data.name;
+    // weather country
+    let country = document.getElementById("country")
+    country.innerHTML= data.sys.country;
+
+
     // weather matric
     let weater =  document.getElementById("temp");
     weater.innerHTML= Math.floor(data.main.temp) + "°c";
@@ -102,32 +103,20 @@ const weather = (data)=>{
     description.innerHTML = data.weather[0].description;
     // weather humidity
     let humidity = document.getElementById("humidity");
-    humidity.innerHTML = "humidity "+data.main.humidity;
+    humidity.innerHTML = "Humidity : "+data.main.humidity + "%";
     // weather temp_min
-    let temp_min = document.getElementById("temp_min")
-    temp_min.innerHTML = "temp min : " + Math.floor(data.main.temp_min)+ "°c";
+    let temp_min = document.getElementById("temp_min");
+    temp_min.innerHTML = "Temp min : " + Math.floor(data.main.temp_min)+ "°c";
     // weather temp_max
-    let temp_max = document.getElementById("temp_max")
-    temp_max.innerHTML = "temp max : " + Math.floor(data.main.temp_max)+ "°c";
+    let temp_max = document.getElementById("temp_max");
+    temp_max.innerHTML = "Temp max : " + Math.floor(data.main.temp_max)+ "°c";
     // weather wind
-    let speed =  document.getElementById("speed")
+    let wind = document.getElementById("wind");
+    wind.innerHTML="Wind:";
+    // weather speed
+    let speed =  document.getElementById("speed");
     speed.innerHTML = "Speed : " + data.wind.speed;
     // weather deg
     let deg =  document.getElementById("deg");
-    deg.innerHTML= "deg : " + data.wind.deg
+    deg.innerHTML= "Deg : " + data.wind.deg;
 }
-
-const weatherContainer = ()=>{
-
-    let container =  document.getElementById("weather-container")
-    container.style.background=  "linear-gradient(to right,rgba(122, 160, 122, 0.796),rgba(255, 227, 201, 0.792))" ;
-    container.style.border= "2px solid #8186c070 ";
-    container.style.width=" 40%" ;
-    container.style.boxSizing = "border-box";
-    container.style.display = "grid";
-    // container.style.gridTemplateRows = "70 px"
-    container.style.gridTemplateColumns = " 100 1fr 1fr 1fr 1fr";
-    container.style.gridGap= "10px";
-
-}
-weatherContainer()
